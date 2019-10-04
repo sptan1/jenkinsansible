aee5c448c81869accd7c45e2a33593f891ef64f4
sptan1 <shu-pei.tan@exxonmobil.com>
2019-10-04 16:47:08 +0800

    update guess_the_number.py to guess result

--- python/large/guess_the_number.py
+++ python/large/guess_the_number.py
@@ -1,11 +1,11 @@
 import random
 print ("I am thinking of a number between 1 and 10.")
 guess = 0
-i = 0
+i = 5
 again = "Y"
 while again == "Y" :
     secret = random.randint(1,10)
-    while guess != secret and i < 5:
+    while guess != secret and i > 0:
         guess = int(input("What's the number? "))
         if guess > secret:
             print(f'{guess} is too high.')
@@ -14,15 +14,18 @@ while again == "Y" :
         elif guess == secret :
             print("Yes! You Win!")
             break
-        i +=1
-        if i== 5:
+        i -=1
+        if i== 0:
             print("You ran out of guesses!")
         
     again = ""
     while again != "Y" and again != "N" :
         again = input("Do you want to play again (Y or N)? ")
         if again == "Y": 
-            i = 0
+            i = 5
+            guess = 0
+            print ("I am thinking of a number between 1 and 10.")
+            print(f'You have {i} guesses left.')
         elif again == "N":
             print("Bye!")
             
