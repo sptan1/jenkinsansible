f16f4f23603aba3741127e183cf0d2d02c9e9597
Tan Shu Pei <shu-pei.tan@exxonmobil.com>
2019-10-09 08:18:50 +0000

    update large exercise

--- python/python102/large/tic_tac_toe.py
+++ python/python102/large/tic_tac_toe.py
@@ -21,20 +21,11 @@ while game == "n":
 
 	print("Player : ", player)
 		
-	count =0
 	for row in board :
-		print(count,row)
-		count +=1
-
-	if player == 1 :
-		player =2
-	elif player == 2 :
-		player =1
-
+		print(row)
 
 	for row in board:
 		if row.count(row[0]) == len(row) and row[0] != 0 :
-			print("Player ",row[0], " is the winner!")
 			game = "y"
 
 	for col in range(len(board[0])):
@@ -42,16 +33,16 @@ while game == "n":
 		for row in board:
 			check.append(row[col])
 		if check.count(check[0])== len(check) and check[0] !=0:
-			print("Player ", check[0], " is the winner!")
 			game = "y"
 	
 	if board[0][0] == board[1][1] and board[0][0]== board[2][2] and board[0][0]!=0 :
-		print("Player ", board[0][0], " is the winner!")
 		game = "y"
 
 	if board[0][2] == board [1][1] and board [0][2] == board[2][0] and board[0][2]!=0:
-		print("Player ", board[0][2], " is the winner!")
 		game="y"
+
+	if game == "y" :
+		print("Player", player, "is the winner!")
 
 	temp = 0
 	if game != "y" :
@@ -59,7 +50,14 @@ while game == "n":
 			for j in range(3):
 				if board[i][j] == 0:
 					temp += 1
-	if temp == 0 and game !="y":
-		print("Game Over!")
 
+	if player == 1 :
+		player = 2
+	elif player == 2:
+		player = 1
+	
 	print("")
+	if temp == 0 and game !="y" :
+		print ("Game Over!!No one win!!")
+		break
+
