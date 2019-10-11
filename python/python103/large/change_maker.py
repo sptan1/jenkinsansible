700c6bfcc1386524b633ebe146b8a2527b624253
Tan Shu Pei <shu-pei.tan@exxonmobil.com>
2019-10-11 09:54:12 +0000

    add python103 large exercise

---
+++ python/python103/large/change_maker.py
@@ -0,0 +1,69 @@
+def bills(bill):
+	hundred = 0
+	fifty = 0
+	twenty =0 
+	ten =0
+	five =0
+	single =0
+
+	if bill >= 100 :
+		hundred = bill //100
+		bill = bill %100
+
+	if bill >= 50:
+		fifty = bill // 50
+		bill = bill % 50
+
+	if bill >= 20:
+		twenty = bill // 20
+		bill = bill % 20
+
+	if bill >= 10:
+		ten = bill //10
+		bill = bill % 10
+
+	if bill >= 5:
+		five = bill//5
+		bill = bill % 5
+	single = bill // 1
+	bill %= 1
+
+	return ( single, five, ten, twenty, fifty, hundred)
+
+def coins(coin):
+	quarter =0
+	dimes =0 
+	nickels = 0
+	pennies = 0
+
+	if coin >=25:
+		quarter = coin // 25
+		coin =coin % 25
+
+	if coin >= 10:
+		dimes = coin //10
+		coin = coin %10
+	
+	if coin >= 5:
+		nickels == coin //5
+	
+	pennies = coin //1
+	coin %= 1
+	
+	return (pennies, nickels, dimes, quarter)
+
+def make_change(total_charge,payment):
+	c = round((payment - total_charge),2)
+	print("The change is :", c)
+	bill = int(c //1)
+	coin = int(str(c).split(".")[1])
+	b_tuple = bills(bill)
+	c_tuple = coins(coin)
+	
+	return (b_tuple,c_tuple)
+
+charge = float(input("Please enter the amount of bill: "))
+pay = float(input("Please enter the payment: "))
+
+change=make_change(charge,pay)
+print("The bills are : {} hundred, {} fifties, {} twenty, {} ten, {} fives, {} singles. The coins are:  {} quarters, {} dimes, {} nickel, {} pennies".format(change[0][5],change[0][4],change[0][3],change[0][2],change[0][1],change[0][0],change[1][3],change[1][2],change[1][1],change[1][0]))
--- python/python103/large/tic_tac_toe.py
+++ python/python103/large/tic_tac_toe.py
@@ -1,6 +1,111 @@
+board = [[" "," "," "],[" "," "," "],[" "," "," "]]
+cont = "yes"
+
+def checkboard(board,location):
+	return (board[location[0]][location[1]] == " ")
+
+def validateLocation(location):
+	while location[0] >2 :
+		s = int(input("Wrong size for row! Please enter 0-2 only: "))
+		location = (s,location[1])
+	while location[1] > 2 :
+		s= int(input("Wrong size for column! Please enter 0-2 only: "))
+		location = (location[0],s)
+	return location
+
+def validatePlayer(player):
+	while player!="Y" and  player!="X" :
+		player = input("Please enter \"X\" or \"Y\": ")
+	return player
+
+
+def b_status(board) :
+	print("   0 1 2")
+	for i in range(len(board[0])):
+		for j in range(len(board[0])):
+			if j==len(board[0])-1 :
+				print("{}|".format(board[i][j]))
+			elif j == 0 :
+				print("{} |{}|".format(i,board[i][j]),end="")
+			else :
+				print("{}|".format(board[i][j]),end="")
+
+
+def inputLocation():
+	x =int(input("Please enter the move for row (0-2) : "))
+	y =int(input("Please enter the move for column (0-2) : "))
+	
+	return (x,y)
+
+def inputPlayer():
+	player = input("DO you want to be X or Y: ")
+	return player
+
+def winner(board):
+	wplay = " "
+	cont = "yes"
+	for row in board :
+		if row.count(row[0]) == len(row) and row[0] != " ":
+			cont = "no"
+			wplay = row[0]
+	
+	for col in range(len(board[0])):
+		check = []
+		for row in board:
+			check.append(row[col])
+		if check.count(check[0]) == len(check) and check[0] != " ":
+			cont = "no"
+			wplay = check[0]
+
+	if board[0][0] == board[1][1] and board[0][0] == board[2][2] and board[0][0] !=" ":
+		cont = "no"
+		wplay=board[0][0]
+	
+	if board[0][2] == board[1][1] and board[0][2] == board [2][0] and board[0][2] !=" ":
+		cont = "no"
+		wplay=board[0][2]
+	
+	return (cont,wplay)
+
+def space(board):
+	temp = 0
+	cont = "yes"
+	for i in range(len(board[0])):
+		for j in range(len(board[0])):
+			if board[i][j] == " ":
+				temp +=1	
+
+	if temp == 0:
+		cont = "stop"
+	return cont
+
 def move(board, location, player):
+	try:
+		location = validateLocation(location)
+		while (not checkboard(board,location)) :
+			print("The location is occupied by {}".format(board[location[0]][location[1]]))
+			location = inputLocation()
+			location = validateLocation(location)
+		player = validatePlayer(player)
+		board[location[0]][location[1]] = player
+		b_status(board)
+			
+	except IndexError:
+		print("Wrong Size! Out of range!")	
+
+b_status(board)
+
+while cont == "yes" :
+	location = inputLocation()
+	player = inputPlayer()
+	move(board,location, player)
+	result = winner(board)	
+	cont = result[0]
+	if cont == "no":
+		print("Player {} is the winner!".format(result[1]))
+	elif cont =="yes":
+		cont=space(board)
+		if cont == "stop":
+			print("Game Over. No winner!")
 
 
-
-
-board = [[0,0,0],[0,0,0],[0,0,0]]
